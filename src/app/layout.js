04476da7485f2545a06e552d5e2ef5ddcda38c93
jtrussell10 <69161168+jtrussell10@users.noreply.github.com>
2024-03-01 21:31:15 +0000

    Added analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react"
 
 export const fontSans = FontSans({
   subsets: ["latin"],
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
         )}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
