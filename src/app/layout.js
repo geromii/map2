13d5cb4d80966182ed6f45037c93abaf4083b034
jtrussell10 <69161168+jtrussell10@users.noreply.github.com>
2025-05-24 01:18:23 +0100

    favicon changes again

--- src/app/layout.js
+++ src/app/layout.js
@@ -51,9 +51,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=10.0, user-scalable=yes" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="apple-touch-icon" href="/favicon/apple-icon.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-icon.png" />
         <link rel="manifest" href="/favicon/site.webmanifest" />
       </head>
       <body
